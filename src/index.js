0e5f12e98ccb0ce6389ab141d8cabd0430e27637
Shriyansh Raut <rautshriyansh@gmail.com>
2026-03-08 14:57:50 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//require("dotenv").config({path: "./.env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+
+dotenv.config({
+  path: "./.env"
+});
+
+const app = express();
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR:", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection failed:", err);
+});
